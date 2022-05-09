1c4156c77e78d2a5618e2d3a97cddef154b44abf
Morris <govlmo91@gmail.com>
2022-05-09 20:59:16 +0900

    blockBody, expressionBody

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,15 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    val list = listOf("1","2")
+
+    println("Call blockBody : " + blockBody(null))
+    println("Call expressionBody : " + expressionBody("morris"))
 }
+
+fun blockBody(name: String?): String {
+    return if(name == "morris") "홍은모" else "누구야"
+}
+
+fun expressionBody(name: String) = if(name == "morris") "홍은모" else "누구야"

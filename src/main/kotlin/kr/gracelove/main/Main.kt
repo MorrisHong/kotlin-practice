ba14c4c34f8c50013af94676fd27eae6b9bb0701
Morris <govlmo91@gmail.com>
2022-05-10 10:47:39 +0900

    뮤터블, 이뮤터블 스트링

--- src/main/kotlin/kr/gracelove/main/Main.kt
+++ src/main/kotlin/kr/gracelove/main/Main.kt
@@ -1,3 +1,5 @@
+package kr.gracelove.main
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
---
+++ src/main/kotlin/kr/gracelove/variable/Variable.kt
@@ -0,0 +1,12 @@
+package kr.gracelove.variable
+
+fun main(args: Array<String>) {
+    val immutableAndTypeInferenceString = "안녕. 나는 그럽이야."
+//     immutableAndTypeInferenceString = "안녕 나는 모리스야." // Val cannot be reassigned
+
+    var mutableAndTypeInferenceString = "안녕 나는 그럽이야."
+    mutableAndTypeInferenceString = "안녕 나는 모리스야."
+
+
+    val immutableString:String
+}

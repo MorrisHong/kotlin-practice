948445ba70b523b0b8da9f2d6a3a1227b9be1c46
Morris <govlmo91@gmail.com>
2022-05-10 09:40:50 +0900

    any

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,14 +5,22 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
-    val list = listOf("1","2")
+    val list = listOf("1", "2")
 
     println("Call blockBody : " + blockBody(null))
     println("Call expressionBody : " + expressionBody("morris"))
 }
 
 fun blockBody(name: String?): String {
-    return if(name == "morris") "홍은모" else "누구야"
+    return if (name == "morris") "홍은모" else "누구야"
 }
 
-fun expressionBody(name: String) = if(name == "morris") "홍은모" else "누구야"
+fun expressionBody(name: String) = if (name == "morris") "홍은모" else "누구야"
+
+
+fun test() = if(true) "" else 123
+
+class temp {
+    fun tempMethod() = expressionBody("morris")
+
+}

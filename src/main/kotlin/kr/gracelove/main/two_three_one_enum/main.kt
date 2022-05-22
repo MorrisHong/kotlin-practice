7a5f64a6f4b111c32a86707591dc4e6b36d14097
Morris <govlmo91@gmail.com>
2022-05-22 13:34:47 +0900

    Enum when

--- src/main/kotlin/kr/gracelove/main/two_three_one_enum/main.kt
+++ src/main/kotlin/kr/gracelove/main/two_three_one_enum/main.kt
@@ -1,6 +1,43 @@
 package kr.gracelove.main.two_three_one_enum
 
 fun main(args: Array<String>) {
-    Color.RED.displayName()
-    Color.GREEN.displayName()
+    println(getMnemonicBody(Color.GREEN))
+    println(getMnemonicExpression(Color.GREEN))
+    println(getMnemonicExpression2(Color.GREEN))
+}
+
+fun getMnemonicBody(color: Color) {
+    when (color) {
+        Color.RED -> "R"
+        Color.ORANGE -> "O"
+        Color.YELLOW -> "Y"
+        Color.GREEN -> "G"
+        Color.BLUE -> "B"
+        Color.INDIGO -> "I"
+        Color.VIOLET -> "V"
+    }
+}
+
+fun getMnemonicExpression(color: Color) = {
+    when (color) {
+        Color.RED -> "R"
+        Color.ORANGE -> "O"
+        Color.YELLOW -> "Y"
+        Color.GREEN -> "G"
+        Color.BLUE -> "B"
+        Color.INDIGO -> "I"
+        Color.VIOLET -> "V"
+    }
+}
+
+fun getMnemonicExpression2(color: Color) = run {
+    when (color) {
+        Color.RED -> "R"
+        Color.ORANGE -> "O"
+        Color.YELLOW -> "Y"
+        Color.GREEN -> "G"
+        Color.BLUE -> "B"
+        Color.INDIGO -> "I"
+        Color.VIOLET -> "V"
+    }
 }

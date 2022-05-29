621098b24d4f2812637d81397e874f50360efd87
Morris <govlmo91@gmail.com>
2022-05-29 16:59:01 +0900

    JoinToString - default parameter value

--- src/main/kotlin/kr/gracelove/main/chapter03/collection/joinToString.kt
+++ src/main/kotlin/kr/gracelove/main/chapter03/collection/joinToString.kt
@@ -1,16 +1,36 @@
 package kr.gracelove.main.chapter03.collection
 
 fun main() {
-    val list = listOf(1,2,3)
+    val list = listOf(1, 2, 3)
     println("origin : $list")
-    println("joinToString : ${joinToString(list,";","(",")")}")
+    println("joinToString : ${joinToString(list, ";", "(", ")")}")
+    println(
+        "joinToString with parameter name : ${
+            joinToString(
+                collection = list,
+                separator = ";",
+                prefix = "(",
+                postfix = ")"
+            )
+        }"
+    )
+    println(
+        """
+        joinToString with default value : 
+        ${joinToString(
+                collection = list, 
+                prefix = "#",
+            )}
+        """
+    )
+
 }
 
 fun <T> joinToString(
     collection: Collection<T>,
-    separator: String,
-    prefix: String,
-    postfix: String,
+    separator: String = ",",
+    prefix: String = "",
+    postfix: String = "",
 ): String {
 
     val result = StringBuilder(prefix)

2f12cf7f16f7cc471223fd5cb547b5f7a12860fc
Morris <govlmo91@gmail.com>
2022-05-29 14:41:42 +0900

    Collection

---
+++ src/main/kotlin/kr/gracelove/main/chapter03/collection/main.kt
@@ -0,0 +1,41 @@
+package kr.gracelove.main.chapter03.collection
+
+fun main() {
+    map()
+    list()
+    set()
+}
+
+fun map() {
+    val mapOf = mapOf(1 to "onee", 2 to "twoo")
+    println("mapOf.javaClass : ${mapOf.javaClass}")
+    println("mapOf : $mapOf")
+
+    val hashMapOf = hashMapOf(1 to "onee", 2 to "twooo")
+    println("hashMapOf.javaClass : ${hashMapOf.javaClass}")
+    println("hashMapOf : $hashMapOf")
+}
+
+fun list() {
+    val listOf = listOf(1, 3, 4)
+    println("listOf.javaClass : ${listOf.javaClass}")
+    println("listOf : $listOf")
+
+    val arrayListOf = arrayListOf(53, 1, 9)
+    println("arrayListOf.javaClass : ${arrayListOf.javaClass}")
+    println("arrayListOf : $arrayListOf")
+}
+
+fun set() {
+    val setOf = setOf("a", "b", "c")
+    println("setOf.javaClass : ${setOf.javaClass}")
+    println("setOf : $setOf")
+
+    val oneSetOf = setOf(1)
+    println("oneSetOf.javaClass : ${oneSetOf.javaClass}")
+    println("oneSetOf : $oneSetOf")
+
+    val hashSetOf = hashSetOf(1, 7, 53)
+    println("hashsetOf.javaClass : ${hashSetOf.javaClass}")
+    println("hashsetOf : $hashSetOf")
+}

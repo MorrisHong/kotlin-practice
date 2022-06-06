4724969b12bcf501eb686f2da635e49e46a12157
Morris <govlmo91@gmail.com>
2022-06-06 18:08:36 +0900

    validate-2

--- src/main/kotlin/kr/gracelove/main/chapter03/user/userService.kt
+++ src/main/kotlin/kr/gracelove/main/chapter03/user/userService.kt
@@ -3,16 +3,17 @@ package kr.gracelove.main.chapter03.user
 
 
 fun saveUser(user: User) {
-    if (user.name.isEmpty()) {
-        throw IllegalArgumentException(
-            "Can't save user id - ${user.id}: empty Name"
-        )
-    }
 
-    if (user.address.isEmpty()) {
-        throw IllegalArgumentException(
-            "Can't save user address - ${user.address}: empty Address"
-        )
+    fun validate(
+        user: User,
+        value: String,
+        fieldName: String,
+    ) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException(
+                "Can't save user ${user.id}: empty $fieldName"
+            )
+        }
     }
 
     println("SUCCEED!")

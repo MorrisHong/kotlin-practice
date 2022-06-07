2aeeeeb21f6fd034b91ca4468214d780ebe81f20
Morris <govlmo91@gmail.com>
2022-06-07 21:34:03 +0900

    WIP

--- src/main/kotlin/kr/gracelove/main/chapter04/interfaces/Button.kt
+++ src/main/kotlin/kr/gracelove/main/chapter04/interfaces/Button.kt
@@ -3,3 +3,22 @@ package kr.gracelove.main.chapter04.interfaces
 class Button : Clickable {
     override fun click() = println("I was clicked")
 }
+
+
+internal fun main() {
+
+}
+
+open class A {
+    open fun method() = println("A")
+}
+
+interface B {
+    fun method() = println("B")
+}
+
+class AB: A(), B {
+    override fun method() {
+
+    }
+}
--- src/main/kotlin/kr/gracelove/main/chapter04/interfaces/main.kt
+++ src/main/kotlin/kr/gracelove/main/chapter04/interfaces/main.kt
@@ -2,8 +2,15 @@ package kr.gracelove.main.chapter04
 
 import kr.gracelove.main.chapter04.interfaces.Button
 import kr.gracelove.main.chapter04.interfaces.Clickable
+import java.util.LinkedList
 
 fun main() {
-    val clickable: Clickable = Button()
-    clickable.click()
+    val listOf = listOf(1, 2, 3, 4)
+    println(listOf.javaClass)
+
+    val of = setOf(1, 2, 3, 4)
+    println(of.javaClass)
+
+    val mapOf = mapOf(1 to 1, 2 to 2)
+    println(mapOf.javaClass)
 }
